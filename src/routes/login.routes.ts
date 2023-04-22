3378f8489d0d762fe630426c0a9a342ffc9e23da
Renato Gouveia <rpgouveia@gmail.com>
2023-04-22 14:52:28 -0300

    fix: update loginRoutes

--- src/routes/login.routes.ts
+++ src/routes/login.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express"
 import { createLoginController } from "../controllers/login.controllers"
+import checkRequestBodyData from "../middlewares/checkRequestBodyData.middleware"
+import { requestLoginSchema } from "../schemas/login.schema"
 
 const loginRoutes: Router = Router()
 
-loginRoutes.post('', createLoginController)
+loginRoutes.post('', checkRequestBodyData(requestLoginSchema), createLoginController)
 
 export default loginRoutes
